234ceb0f2bbe068216ecf050b99a297f9808a3e5
unknown <alfredom@ALFREDOM.na.qualcomm.com>
2015-09-30 15:09:47 -0700

    Implemented OkHttpClient to support Http2

---
+++ agent/ok_http_client/src/test/java/com/intuit/tank/okhttpclient/MockBaseRequest.java
@@ -0,0 +1,26 @@
+package com.intuit.tank.okhttpclient;
+
+import com.intuit.tank.http.BaseRequest;
+import com.intuit.tank.http.TankHttpClient;
+
+public class MockBaseRequest extends BaseRequest {
+
+    protected MockBaseRequest(TankHttpClient httpclient) {
+        super(httpclient, new MockLogUtil());
+    }
+
+    @Override
+    public void setKey(String key, String value) {
+    }
+
+    @Override
+    public String getKey(String key) {
+        return null;
+    }
+
+    @Override
+    public void setNamespace(String name, String value) {
+
+    }
+
+}
