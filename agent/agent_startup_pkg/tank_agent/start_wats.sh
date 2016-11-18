802355d27bfd1330eeae90f482965841c9599863
Kevin McGoldrick <kevin_mcgoldrick@intuit.com>
2016-11-18 14:57:41 -0800

    Revert "adjust DNS cache settings"
    This reverts commit 6ed4c8be85335bf25a496a463cb723cd3856e670.

--- agent/agent_startup_pkg/tank_agent/start_wats.sh
+++ agent/agent_startup_pkg/tank_agent/start_wats.sh
@@ -1,4 +1,4 @@
 #!/bin/sh                                                                                                                                                                                                                                    
-vm_arguments="-Dnetworkaddress.cache.ttl=5 networkaddress.cache.negative.ttl=0";
+vm_arguments="-Dnetworkaddress.cache.ttl=0";
 java -cp apiharness-1.0-all.jar $2 $vm_arguments com.intuit.tank.harness.APITestHarness $1
 echo DONE
