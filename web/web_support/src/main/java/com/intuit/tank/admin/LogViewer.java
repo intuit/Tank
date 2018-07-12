e37930453bbb01cd258c3874221a706396538e01
Kevin McGoldrick <kevin_mcgoldrick@intuit.com>
2018-07-12 14:10:39 -0700

    Debug Log Viewer

--- web/web_support/src/main/java/com/intuit/tank/admin/LogViewer.java
+++ web/web_support/src/main/java/com/intuit/tank/admin/LogViewer.java
@@ -86,10 +86,11 @@ public class LogViewer implements Serializable {
                         + ReportService.SERVICE_RELATIVE_PATH + File.separator
                         + URLEncoder.encode(currentLogFile, StandardCharsets.UTF_8.toString());
             } catch (UnsupportedEncodingException e) {
+                return "error " + contextPath + " error";
                 // never happens stupid exception
             }
         }
-        return null;
+        return "empty";
     }
 
     /**
