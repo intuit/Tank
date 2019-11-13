8bfe92ef976b03e818647d3266ef150517e26c9e
Kevin McGoldrick <kevin_mcgoldrick@intuit.com>
2019-11-12 16:00:49 -0800

    fix naming overlap

--- web/web_support/src/main/java/com/intuit/tank/service/FilterService.java
+++ web/web_support/src/main/java/com/intuit/tank/service/FilterService.java
@@ -32,7 +32,7 @@ import com.intuit.tank.project.ScriptFilter;
  * @author dangleton
  * 
  */
-@Named
+@Named("scriptFilterService")
 @Dependent
 public class FilterService implements Serializable {
 
