50f85920df51149d4145c1d134ee270774b16dab
djagaluru <DarshanSiddesh_Jagaluru@intuit.com>
2020-10-25 23:12:18 +0530

    Improving test coverage for data-model project module

--- data_model/src/test/java/com/intuit/tank/project/SummaryDataBuilderTest.java
+++ data_model/src/test/java/com/intuit/tank/project/SummaryDataBuilderTest.java
@@ -21,6 +21,8 @@ import org.junit.jupiter.api.Test;
 import com.intuit.tank.project.SummaryData;
 import com.intuit.tank.project.SummaryDataBuilder;
 
+import java.util.Date;
+
 /**
  * The class <code>SummaryDataBuilderTest</code> contains tests for the class <code>{@link SummaryDataBuilder}</code>.
  *
@@ -82,6 +84,66 @@ public class SummaryDataBuilderTest {
         assertEquals(0, result.getId());
         assertEquals(null, result.getModified());
         assertEquals(null, result.getCreated());
+    }
+
+    @Test
+    public void testBuild_2() {
+        SummaryDataBuilder builder = new SummaryDataBuilder();
+
+        builder.withJobId(1);
+        builder.withPageId("pageId");
+        builder.withSampleSize(2);
+        builder.withMean(3.0);
+        builder.withMin(4.0);
+        builder.withMax(5.0);
+        builder.withSttDev(6.0);
+        builder.withKurtosis(7.0);
+        builder.withSkewness(8.0);
+        builder.withVarience(9.0);
+        builder.withPercentile10(10.0);
+        builder.withPercentile20(11.0);
+        builder.withPercentile30(12.0);
+        builder.withPercentile40(12.0);
+        builder.withPercentile50(12.0);
+        builder.withPercentile60(12.0);
+        builder.withPercentile70(12.0);
+        builder.withPercentile80(12.0);
+        builder.withPercentile90(12.0);
+        builder.withPercentile95(12.0);
+        builder.withPercentile99(12.0);
+        builder.withId(13);
+        Date date = new Date();
+        builder.withCreated(date);
+        builder.withModified(date);
+        builder.withForceCreateDate(date);
+
+        SummaryData result = builder.build();
+
+        assertNotNull(result);
+        assertEquals("pageId", result.getPageId());
+        assertEquals(5.0, result.getMax());
+        assertEquals(3.0, result.getMean());
+        assertEquals(1, result.getJobId());
+        assertEquals(9.0, result.getVarience());
+        assertEquals(8.0, result.getSkewness());
+        assertEquals(10.0, result.getPercentile10());
+        assertEquals(12.0, result.getPercentile70());
+        assertEquals(11.0, result.getPercentile20());
+        assertEquals(6.0, result.getSttDev(), 1.0);
+        assertEquals(2, result.getSampleSize());
+        assertEquals(7.0, result.getKurtosis(), 1.0);
+        assertEquals(12.0, result.getPercentile80(), 1.0);
+        assertEquals(12.0, result.getPercentile60(), 1.0);
+        assertEquals(12.0, result.getPercentile90(), 1.0);
+        assertEquals(12.0, result.getPercentile50(), 1.0);
+        assertEquals(12.0, result.getPercentile95(), 1.0);
+        assertEquals(12.0, result.getPercentile99(), 1.0);
+        assertEquals(12.0, result.getPercentile40(), 1.0);
+        assertEquals(12.0, result.getPercentile30(), 1.0);
+        assertEquals(4.0, result.getMin(), 1.0);
+        assertEquals(13, result.getId());
+        assertEquals(date, result.getModified());
+        assertEquals(date, result.getCreated());
     }
 
     /**
