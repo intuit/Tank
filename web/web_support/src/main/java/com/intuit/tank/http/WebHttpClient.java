68b2a73277289187f69ed023166107da43bac8e2
Shawn Park <shawn_park@intuit.com>
2021-10-05 11:44:32 -0700

    Remove Http Client Injector given Static builder

--- web/web_support/src/main/java/com/intuit/tank/http/WebHttpClient.java
+++ web/web_support/src/main/java/com/intuit/tank/http/WebHttpClient.java
@@ -3,7 +3,6 @@ package com.intuit.tank.http;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.inject.Inject;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -21,7 +20,6 @@ import java.util.Map;
 public class WebHttpClient {
     private static final Logger LOG = LogManager.getLogger(WebHttpClient.class);
 
-    @Inject
     private HttpClient _httpclient;
 
     public WebHttpClient() {
