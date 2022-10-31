452d2084478a17a2cc361deefbeda6f3fd97bba9
srujana <csrujana94@gmail.com>
2022-10-31 22:22:55 +0530

    Removed Comment block

--- tank_vmManager/src/test/java/com/intuit/tank/vmManager/environment/amazon/CloudwatchInstanceTest.java
+++ tank_vmManager/src/test/java/com/intuit/tank/vmManager/environment/amazon/CloudwatchInstanceTest.java
@@ -7,9 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-/**
- * @author Srujana Cheruvu
- */
 public class CloudwatchInstanceTest {
 
     @Test
