daa4d8cb7e57467e078378cae985df31c2153711
srujana4 <csrujana94@gmail.com>
2022-10-31 22:24:14 +0530

    Added Test Case for tank_vmManager to increase coverage from 9% to 10% for issue #88 (#224)
    * Added Test Case for tank_vmManager to increase coverage from 9% to 10% for issue #88
    authored by csrujana94@gmail.com

---
+++ tank_vmManager/src/test/java/com/intuit/tank/vmManager/environment/amazon/CloudwatchInstanceTest.java
@@ -0,0 +1,19 @@
+package com.intuit.tank.vmManager.environment.amazon;
+
+import com.intuit.tank.test.TestGroups;
+import com.intuit.tank.vm.api.enumerated.VMRegion;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CloudwatchInstanceTest {
+
+    @Test
+    @Tag(TestGroups.FUNCTIONAL)
+    public void testCloudwatchInstance() {
+        CloudwatchInstance cloudwatchInstance = new CloudwatchInstance(VMRegion.STANDALONE);
+        assertNotNull(cloudwatchInstance);
+    }
+
+}
