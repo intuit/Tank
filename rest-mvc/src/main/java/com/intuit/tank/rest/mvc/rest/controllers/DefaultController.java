c1247bc3f8727d62fbe11d82ca323581e738e6ff
zkofiro <zakaria_kofiro@intuit.com>
2023-06-09 00:35:54 -0700

    reverting defaultcontroller

--- rest-mvc/src/main/java/com/intuit/tank/rest/mvc/rest/controllers/DefaultController.java
+++ rest-mvc/src/main/java/com/intuit/tank/rest/mvc/rest/controllers/DefaultController.java
@@ -7,30 +7,6 @@
  */
 package com.intuit.tank.rest.mvc.rest.controllers;
 
-import com.intuit.tank.rest.mvc.rest.clients.AgentClient;
-import com.intuit.tank.api.model.v1.cloud.VMStatus;
-import com.intuit.tank.vm.agent.messages.AgentData;
-import com.intuit.tank.vm.agent.messages.AgentTestStartData;
-import com.intuit.tank.api.model.v1.cloud.CloudVmStatus;
-import com.intuit.tank.vm.api.enumerated.JobStatus;
-import com.intuit.tank.vm.api.enumerated.VMRegion;
-import com.intuit.tank.vm.api.enumerated.VMImageType;
-import org.springframework.web.reactive.function.client.WebClient;
-import com.intuit.tank.rest.mvc.rest.clients.DataFileClient;
-import com.intuit.tank.rest.mvc.rest.clients.ProjectClient;
-import com.intuit.tank.rest.mvc.rest.clients.ScriptClient;
-import com.intuit.tank.api.model.v1.cloud.ValidationStatus;
-import com.intuit.tank.vm.agent.messages.AgentAvailability;
-import com.intuit.tank.vm.agent.messages.AgentAvailabilityStatus;
-import com.intuit.tank.rest.mvc.rest.models.agent.TankHttpClientDefinitionContainer;
-import com.intuit.tank.rest.mvc.rest.models.datafiles.DataFileDescriptor;
-import com.intuit.tank.rest.mvc.rest.models.projects.ProjectContainer;
-import com.intuit.tank.rest.mvc.rest.models.scripts.ExternalScriptTO;
-import com.intuit.tank.rest.mvc.rest.models.scripts.ScriptDescriptionContainer;
-
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import org.apache.commons.io.IOUtils;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -41,20 +17,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-import java.util.Map;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 @RestController
 @Tag(name = "Default")
 public class DefaultController {
-
-    private ScriptClient scriptClient;
-    private AgentClient agentClient;
-    private ProjectClient projectClient;
-    private DataFileClient dataFileClient;
 
     @GetMapping(value ="/v2/ping",  produces = { MediaType.TEXT_PLAIN_VALUE } )
     @Operation(description = "Pings Tank V2 API default service", summary = "Check if Tank V2 API is up")
@@ -65,22 +30,4 @@ public class DefaultController {
         return new ResponseEntity<>("PONG Tank V2 API", HttpStatus.OK);
     }
 
-    @GetMapping(value ="/v2/pong",  produces = { MediaType.TEXT_PLAIN_VALUE } )
-    public String pong() {
-        this.dataFileClient = new  DataFileClient("http://localhost:8080/tank_war");
-
-//        String result = WebClient.create("http://localhost:8080/tank_war/api/v2/datafiles")
-//                .get()
-//                .uri(uriBuilder -> uriBuilder.path("/content")
-//                        .queryParam("id", "24")
-//                        .build())
-//                .retrieve()
-//                .bodyToMono(String.class)
-//                .block();
-
-
-        InputStream is = IOUtils.toInputStream(dataFileClient.getDatafileContent(24), StandardCharsets.UTF_8);
-
-        return dataFileClient.getDatafileContent(24);
-    }
 }
