37fec501cc94037b1cea2dc5ac8c9b8ce3bd562d
zkofiro <zakaria_kofiro@intuit.com>
2023-09-26 20:04:49 -0700

    reverting - steady state should work w/ requests

--- agent/apiharness/src/main/java/com/intuit/tank/harness/TestPlanStarter.java
+++ agent/apiharness/src/main/java/com/intuit/tank/harness/TestPlanStarter.java
@@ -240,19 +240,18 @@ public class TestPlanStarter implements Runnable {
                 // start rest of users sleeping between each interval
                 while (!done) {
 
-//                    // add additional fractional users to the total users during ramp time
-//                    double currentUsers = calculateTotalUsers(agentTimer);
-//                    double expectedUsersToAdd = currentUsers - previousTotalUsers;
-//                    previousTotalUsers = currentUsers;
-//
-//                    if(expectedUsersToAdd >= 1 && expectedUsersToAdd < 2) {
-//                        accumulatedUsers += (expectedUsersToAdd - 1);
-//                        if (accumulatedUsers >= 1) {
-//                            createThread(httpClient, this.threadsStarted);
-//                            additionalUsers++;
-//                            accumulatedUsers -= 1;
-//                        }
-//                    }
+                    // add additional fractional users to the total users during ramp time
+                    double currentUsers = calculateTotalUsers(agentTimer);
+                    double expectedUsersToAdd = currentUsers - previousTotalUsers;
+                    previousTotalUsers = currentUsers;
+
+                    if(expectedUsersToAdd >= 1 && expectedUsersToAdd < 2) {
+                        accumulatedUsers += (expectedUsersToAdd - 1);
+                        if (accumulatedUsers >= 1) {
+                            createThread(httpClient, this.threadsStarted);
+                            accumulatedUsers -= 1;
+                        }
+                    }
 
                     while (APITestHarness.getInstance().getCmd() == AgentCommand.pause_ramp
                             || APITestHarness.getInstance().getCmd() == AgentCommand.pause) {
@@ -325,6 +324,7 @@ public class TestPlanStarter implements Runnable {
                         if(currentRampRate < agentRunData.getTargetRampRate()) {
                             currentRampRate++;
                             lastRampIncreaseTime = currentTime;
+                            LOG.info("Nonlinear - Increasing ramp rate to " + currentRampRate + " users/sec for instance " + agentRunData.getInstanceId());
                         }
                     }
 
