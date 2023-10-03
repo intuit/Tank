748b33c7e6798f574d81c6edc9784cfe572f9bdb
zkofiro <zakaria_kofiro@intuit.com>
2023-10-02 17:25:15 -0700

    set thread context only if running in AWS

--- api/src/main/java/com/intuit/tank/logging/LoggingConfig.java
+++ api/src/main/java/com/intuit/tank/logging/LoggingConfig.java
@@ -6,14 +6,16 @@ import com.intuit.tank.harness.HostInfo;
 
 public class LoggingConfig {
     public static void setupThreadContext() {
-        HostInfo hostInfo = new HostInfo();
-        ThreadContext.put("jobId", AmazonUtil.getJobId());
-        ThreadContext.put("projectName", AmazonUtil.getProjectName());
-        ThreadContext.put("instanceId", AmazonUtil.getInstanceId());
-        ThreadContext.put("publicIp", hostInfo.getPublicIp());
-        ThreadContext.put("location", AmazonUtil.getZone());
-        ThreadContext.put("httpHost", AmazonUtil.getControllerBaseUrl());
-        ThreadContext.put("loggingProfile", AmazonUtil.getLoggingProfile().getDisplayName());
+        if(AmazonUtil.isInAmazon()) {
+            HostInfo hostInfo = new HostInfo();
+            ThreadContext.put("jobId", AmazonUtil.getJobId());
+            ThreadContext.put("projectName", AmazonUtil.getProjectName());
+            ThreadContext.put("instanceId", AmazonUtil.getInstanceId());
+            ThreadContext.put("publicIp", hostInfo.getPublicIp());
+            ThreadContext.put("location", AmazonUtil.getZone());
+            ThreadContext.put("httpHost", AmazonUtil.getControllerBaseUrl());
+            ThreadContext.put("loggingProfile", AmazonUtil.getLoggingProfile().getDisplayName());
+        }
     }
 
     public static void clearThreadContext() {
