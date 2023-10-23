2aeee333a6ba8ed71967e0fbf83d636e11ede606
zkofiro <zakaria_kofiro@intuit.com>
2023-10-23 15:23:10 -0700

    move file upload resources to try-with-resources to autoclose at end of statement

--- rest-mvc/src/main/java/com/intuit/tank/rest/mvc/rest/services/datafiles/DataFileServiceV2Impl.java
+++ rest-mvc/src/main/java/com/intuit/tank/rest/mvc/rest/services/datafiles/DataFileServiceV2Impl.java
@@ -151,10 +151,8 @@ public class DataFileServiceV2Impl implements DataFileServiceV2 {
         Map<String, String> payload = new HashMap<>();
         datafileId = datafileId == null ? 0 : datafileId;
         contentEncoding = contentEncoding == null ? "" : contentEncoding;
-        BufferedReader bufferedReader = StringUtils.equalsIgnoreCase(contentEncoding, "gzip") ?
-                new BufferedReader(new InputStreamReader(new GZIPInputStream(file.getInputStream()))) :
-                new BufferedReader(new InputStreamReader(file.getInputStream()));
-        try (InputStream decompressed = IOUtils.toInputStream(IOUtils.toString(bufferedReader), StandardCharsets.UTF_8)) {
+        try (InputStream fileInputStream = file.getInputStream();
+             InputStream decompressed = "gzip".equalsIgnoreCase(contentEncoding) ? new GZIPInputStream(fileInputStream) : fileInputStream) {
             DataFileDao dao = new DataFileDao();
             DataFile dataFile = dao.findById(datafileId);
             if (dataFile == null) {
@@ -179,12 +177,6 @@ public class DataFileServiceV2Impl implements DataFileServiceV2 {
                 }
             }
             payload.put("datafileId", Integer.toString(dataFile.getId()));
-            try {
-                decompressed.close();
-            } catch (IOException e) {
-                LOGGER.error("Error uploading datafile: " + e.getMessage(), e);
-                throw new GenericServiceCreateOrUpdateException("datafiles", "new datafile via datafile upload", e);
-            }
         } catch (Exception e) {
             LOGGER.error("Error uploading datafile: " + e.getMessage(), e);
             throw new GenericServiceCreateOrUpdateException("datafiles", "new datafile via datafile upload", e);
