07fba09423b0f6c5f7534c6f0cb824f199b66689
zkofiro <zakaria_kofiro@intuit.com>
2024-12-11 11:24:23 -0800

    updating

--- agent/agent_startup/src/main/java/com/intuit/tank/agent/AgentStartup.java
+++ agent/agent_startup/src/main/java/com/intuit/tank/agent/AgentStartup.java
@@ -20,9 +20,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipInputStream;
@@ -100,12 +101,9 @@ public class AgentStartup implements Runnable {
             String jvmArgs = AmazonUtil.getUserDataAsMap().get(TankConstants.KEY_JVM_ARGS);
             logger.info("Starting apiharness with command: {} {} {}",
                     API_HARNESS_COMMAND, controllerArg, jvmArgs);
-            List<String> command = new ArrayList<>();
-            command.add(API_HARNESS_COMMAND);
-            command.add(controllerArg);
+            List<String> command = Stream.of(API_HARNESS_COMMAND, controllerArg).collect(Collectors.toList());
             command.addAll(Arrays.asList((jvmArgs).split("\\s+")));
-            ProcessBuilder processBuilder = new ProcessBuilder(command);
-            processBuilder.start();
+            new ProcessBuilder(command).start();
         } catch (ConnectException ce) {
             logger.error("Error creating connection to {} : this is normal during the bake : {}",
                     controllerBaseUrl, ce.getMessage());
