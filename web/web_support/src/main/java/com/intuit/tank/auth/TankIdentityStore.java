2693295a094da7fe599f0a9ef71a1b5ce0bfe2d9
Kevin McGoldrick <kevin_mcgoldrick@intuit.com>
2025-03-05 14:16:14 -0800

    Revert "stream userRoles"
    This reverts commit 236fef6ea2ab677e7e03a419bd6e97debef55ace.

--- web/web_support/src/main/java/com/intuit/tank/auth/TankIdentityStore.java
+++ web/web_support/src/main/java/com/intuit/tank/auth/TankIdentityStore.java
@@ -15,8 +15,9 @@ import jakarta.security.enterprise.credential.Credential;
 import jakarta.security.enterprise.credential.UsernamePasswordCredential;
 import jakarta.security.enterprise.identitystore.CredentialValidationResult;
 import jakarta.security.enterprise.identitystore.IdentityStore;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 @ApplicationScoped
 public class TankIdentityStore implements IdentityStore {
@@ -39,25 +40,32 @@ public class TankIdentityStore implements IdentityStore {
 
         UsernamePasswordCredential login = (UsernamePasswordCredential) credential;
 
-        LOG.info("Attempting to login {}", login.getCaller());
+        LOG.info("Attempting to login " + login.getCaller());
         User user = new UserDao().authenticate(login.getCaller(), login.getPasswordAsString());
         if (user != null) {
-            Set<String> userRoles = user.getGroups().stream().map(Group::getName).collect(Collectors.toSet());
+            List<String> userRoles = new ArrayList<>();
+            for (Group g : user.getGroups()) {
+                userRoles.add(g.getName());
+            }
             loginEventSrc.fire(user);
-            LOG.info("Successfully logged in {}", login.getCaller());
-            return new CredentialValidationResult(login.getCaller(), userRoles);
+            LOG.info("Successfully logged in " + login.getCaller());
+            return new CredentialValidationResult(login.getCaller(), new HashSet<>(userRoles));
         }
-        LOG.warn("Failed to login {}", login.getCaller());
+        LOG.warn("Failed to login " + login.getCaller());
         return CredentialValidationResult.INVALID_RESULT;
     }
 
     public CredentialValidationResult validateSSOUser(User user) {
         try {
-            Set<String> userRoles = user.getGroups().stream().map(Group::getName).collect(Collectors.toSet());
+            List<String> userRoles = new ArrayList<>();
+            for (Group g : user.getGroups()) {
+                userRoles.add(g.getName());
+            }
             loginEventSrc.fire(user);
-            return new CredentialValidationResult(user.getName(), userRoles);
+
+            return new CredentialValidationResult(user.getName(), new HashSet<>(userRoles));
         } catch(Exception e) {
-            LOG.warn("Failed to login {}", user.getName());
+            LOG.warn("Failed to login " + user.getName());
             return CredentialValidationResult.INVALID_RESULT;
         }
     }
