3a1ab7c65052e09e2cd2c6cbf2f21a49179dd4a9
zkofiro <zakaria_kofiro@intuit.com>
2025-03-20 18:01:11 -0700

    exception handling + update to one info log for agent termination

--- tank_vmManager/src/main/java/com/intuit/tank/vmManager/environment/VMChannelImpl.java
+++ tank_vmManager/src/main/java/com/intuit/tank/vmManager/environment/VMChannelImpl.java
@@ -48,11 +48,16 @@ public class VMChannelImpl implements VMChannel {
      */
     @Override
     public List<VMInformation> findInstancesOfType(VMRegion region, VMImageType type) {
-        LOG.debug("Finding instances of type: " + type + " in region: " + region);
-        AmazonInstance amazonInstance = new AmazonInstance(region);
-        List<VMInformation> instances = amazonInstance.findInstancesOfType(region, type);
-        LOG.debug("Found " + instances.size() + " instances of type: " + type + " in region: " + region);
-        return instances;
+        LOG.debug("Finding instances of type: {} in region: {}", type, region);
+        try {
+            AmazonInstance amazonInstance = new AmazonInstance(region);
+            List<VMInformation> instances = amazonInstance.findInstancesOfType(region, type);
+            LOG.debug("Found {} instances of type: {} in region: {}", instances.size(), type, region);
+            return instances;
+        } catch (Exception e) {
+            LOG.error("Error finding instances of type: {} in region: {}", type, region, e);
+            return List.of();
+        }
     }
 
     /**
@@ -60,12 +65,16 @@ public class VMChannelImpl implements VMChannel {
      */
     @Override
     public void terminateInstances(@Nonnull List<String> instanceIds) {
-        LOG.info("Terminating instances: " + instanceIds);
-        for (VMRegion region : new TankConfig().getVmManagerConfig().getRegions()) {
-            LOG.debug("Terminating instances in region: " + region);
-            new AmazonInstance(region).killInstances(instanceIds);
+        LOG.debug("Terminating instances: {}", instanceIds);
+        try {
+            for (VMRegion region : new TankConfig().getVmManagerConfig().getRegions()) {
+                LOG.debug("Terminating instances in region: {}", region);
+                new AmazonInstance(region).killInstances(instanceIds);
+            }
+            LOG.info("Finished terminating instances: {}", instanceIds);
+        } catch (Exception e) {
+            LOG.error("Error terminating instances: {}", instanceIds, e);
         }
-        LOG.info("Finished terminating instances: " + instanceIds);
     }
 
     /**
@@ -73,12 +82,16 @@ public class VMChannelImpl implements VMChannel {
      */
     @Override
     public void stopInstances(@Nonnull List<String> instanceIds) {
-        LOG.info("Stopping instances: " + instanceIds);
-        for (VMRegion region : new TankConfig().getVmManagerConfig().getRegions()) {
-            LOG.debug("Stopping instances in region: " + region);
-            new AmazonInstance(region).stopInstances(instanceIds);
+        LOG.debug("Stopping instances: {}", instanceIds);
+        try {
+            for (VMRegion region : new TankConfig().getVmManagerConfig().getRegions()) {
+                LOG.debug("Stopping instances in region: {}", region);
+                new AmazonInstance(region).stopInstances(instanceIds);
+            }
+            LOG.debug("Finished stopping instances: {}", instanceIds);
+        } catch (Exception e) {
+            LOG.error("Error stopping instances: {}", instanceIds, e);
         }
-        LOG.info("Finished stopping instances: " + instanceIds);
     }
 
     /**
@@ -86,10 +99,15 @@ public class VMChannelImpl implements VMChannel {
      */
     @Override
     public List<VMInformation> startInstances(@Nonnull VMInstanceRequest request) {
-        LOG.info("Starting instances with request: " + request);
-        List<VMInformation> instances = new AmazonInstance(request.getRegion()).create(request);
-        LOG.debug("Started " + instances.size() + " instances with request: " + request);
-        return instances;
+        LOG.debug("Starting instances with request: {}", request);
+        try {
+            List<VMInformation> instances = new AmazonInstance(request.getRegion()).create(request);
+            LOG.debug("Started {} instances with request: {}", instances.size(), request);
+            return instances;
+        } catch (Exception e) {
+            LOG.error("Error starting instances with request: {}", request, e);
+            return List.of(); // Return an empty list or handle the error appropriately
+        }
     }
 
 }
