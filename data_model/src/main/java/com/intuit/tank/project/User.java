1a2e749f99ed5de718b888b506906c7754de9581
zkofiro <zakaria_kofiro@intuit.com>
2025-08-14 15:31:26 -0700

    update index to only user + token

--- data_model/src/main/java/com/intuit/tank/project/User.java
+++ data_model/src/main/java/com/intuit/tank/project/User.java
@@ -50,7 +50,7 @@ import org.hibernate.envers.Audited;
 @Audited
 @Table(name = "user",
         indexes = { @Index(name = "IDX_USER_NAME", columnList = User.PROPERTY_NAME),
-                    @Index(name = "IDX_USER_LAST_LOGIN", columnList = "last_login_ts")})
+                    @Index(name = "IDX_USER_TOKEN", columnList = "token"),
 
 public class User extends BaseEntity {
 
