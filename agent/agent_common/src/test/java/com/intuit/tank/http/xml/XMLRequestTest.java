addc75e66fc7ff67943ad3e2555c27a0aee6bf69
Kevin McGoldrick <kevin_mcgoldrick@intuit.com>
2025-10-30 12:58:08 -0700

    Update XMLHandler

--- agent/agent_common/src/test/java/com/intuit/tank/http/xml/XMLRequestTest.java
+++ agent/agent_common/src/test/java/com/intuit/tank/http/xml/XMLRequestTest.java
@@ -5,42 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * The class <code>XMLRequestTest</code> contains tests for the class <code>{@link XMLRequest}</code>.
- * 
- * @generatedBy CodePro at 12/16/14 4:29 PM
- */
 public class XMLRequestTest {
     /**
      * Run the XMLRequest(Httpnull) constructor test.
-     * 
-     * @throws Exception
-     * 
-     * @generatedBy CodePro at 12/16/14 4:29 PM
      */
     @Test
-    public void testXMLRequest_1()
-            throws Exception {
-        
-
+    public void testXMLRequest_1() {
         XMLRequest result = new XMLRequest(null, null);
 
-        // An unexpected exception was thrown in user code while executing this test:
-        // java.lang.ExceptionInInitializerError
         assertNotNull(result);
     }
-
    
     /**
      * Run the String getKey(String) method test.
-     * 
-     * @throws Exception
-     * 
-     * @generatedBy CodePro at 12/16/14 4:29 PM
      */
     @Test
-    public void testGetKey_1()
-            throws Exception {
+    public void testGetKey_1() {
         XMLRequest fixture = new XMLRequest(null, null);
         fixture.handler = new GenericXMLHandler();
         String key = "";
@@ -52,22 +32,20 @@ public class XMLRequestTest {
     @Test
     public void testGetKey_2() {
         XMLRequest fixture = new XMLRequest(null, null);
-        fixture.handler = new GenericXMLHandler();
+        fixture.handler = new GenericXMLHandler("<testKey></testKey>");
         fixture.setKey("testKey", "testValue");
-        String result = fixture.getKey("testValue");
-        assertEquals("", result);
+
+        String result = fixture.getKey("testKey");
+
+        assertNotNull(result);
+        assertEquals("testValue", result);
     }
 
     /**
      * Run the void setNamespace(String,String) method test.
-     * 
-     * @throws Exception
-     * 
-     * @generatedBy CodePro at 12/16/14 4:29 PM
      */
     @Test
-    public void testSetNamespace_1()
-            throws Exception {
+    public void testSetNamespace_1() {
         XMLRequest fixture = new XMLRequest(null, null);
         fixture.handler = new GenericXMLHandler();
         String name = "";
